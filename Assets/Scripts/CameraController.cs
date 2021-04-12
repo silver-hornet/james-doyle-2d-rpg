35a9d4712c37d777e482c6da24a6b11176e8db28
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-04-12 08:41:19 -0400

    Completed Lesson 20

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    void Start()
+    {
+        target = PlayerController.instance.transform;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
