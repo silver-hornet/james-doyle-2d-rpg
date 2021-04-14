459d9d998862c32546012ea9a094efdde4965f89
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-04-14 07:32:00 -0400

    Completed Lesson 24

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour
 {
     public Transform target;
 
+    public Tilemap theMap;
+    Vector3 bottomLeftLimit;
+    Vector3 topRightLimit;
+
     void Start()
     {
         target = PlayerController.instance.transform;
+
+        bottomLeftLimit = theMap.localBounds.min;
+        topRightLimit = theMap.localBounds.max;
     }
 
     void LateUpdate()
     {
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        // keep the camera inside the bounds
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
     }
 }
