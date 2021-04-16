bc48174db12b72ac5874f1ac02f38e80a210ff1a
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-04-16 08:21:04 -0400

    Completed Lesson 38

---
+++ Assets/Scripts/CharStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+    public string charName;
+    public int playerLevel = 1;
+    public int currentEXP;
+
+
+
+    public int currentHP;
+    public int maxHP = 100;
+    public int currentMP;
+    public int maxMP = 30;
+    public int strength;
+    public int defence;
+    public int weaponPower;
+    public int armorPower;
+    public string equippedWeapon;
+    public string equippedArmor;
+    public Sprite charImage;
+}
