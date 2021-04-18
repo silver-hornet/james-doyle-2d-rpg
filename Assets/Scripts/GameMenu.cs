e9cf1c46827b539c4542d30052d4c07d9e303cf6
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-04-18 14:30:34 -0400

    Completed Lesson 45

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour
+{
+    public GameObject theMenu;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (theMenu.activeInHierarchy)
+            {
+                theMenu.SetActive(false);
+            }
+            else
+            {
+                theMenu.SetActive(true);
+            }
+        }
+    }
+}
