ad26ba8b92acf2745071225e99c80710f922db71
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-04-19 08:54:35 -0400

    Completed Lesson 47

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour
 {
     public GameObject theMenu;
+
+    CharStats[] playerStats;
+
+    public Text[] nameText;
+    public Text[] hpText;
+    public Text[] mpText;
+    public Text[] lvlText;
+    public Text[] expText;
+    public Slider[] expSlider;
+    public Image[] charImage;
+    public GameObject[] charStatHolder;
 
     void Update()
     {
@@ -18,7 +30,25 @@ public class GameMenu : MonoBehaviour
             else
             {
                 theMenu.SetActive(true);
+                UpdateMainStats();
                 GameManager.instance.gameMenuOpen = true;
+            }
+        }
+    }
+
+    public void UpdateMainStats()
+    {
+        playerStats = GameManager.instance.playerStats;
+
+        for (int i = 0; i < playerStats.Length; i++)
+        {
+            if (playerStats[i].gameObject.activeInHierarchy)
+            {
+                charStatHolder[i].SetActive(true);
+            }
+            else
+            {
+                charStatHolder[i].SetActive(false);
             }
         }
     }
