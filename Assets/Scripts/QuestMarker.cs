839c562ec2e7ae79d1a21a9556393e2ffc97a65c
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-04-28 08:30:38 -0400

    Completed Lesson 75

---
+++ Assets/Scripts/QuestMarker.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestMarker : MonoBehaviour
+{
+    public string questToMark;
+    public bool markComplete;
+
+    public bool markOnEnter;
+    bool canMark;
+
+    public bool deactivateOnMarking;
+
+    void Update()
+    {
+        if (canMark && Input.GetButtonDown("Fire1"))
+        {
+            canMark = false;
+            MarkQuest();
+        }
+    }
+
+    public void MarkQuest()
+    {
+        if (markComplete)
+        {
+            QuestManager.instance.MarkQuestComplete(questToMark);
+        }
+        else
+        {
+            QuestManager.instance.MarkQuestIncomplete(questToMark);
+        }
+
+        gameObject.SetActive(!deactivateOnMarking);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (markOnEnter)
+            {
+                MarkQuest();
+            }
+            else
+            {
+                canMark = true;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canMark = false;
+        }
+    }
+
+}
