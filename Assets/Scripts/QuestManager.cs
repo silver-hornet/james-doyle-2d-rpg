abc3ca5420eb463e69948495811c03675d662813
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-04-28 08:01:58 -0400

    Completed Lesson 72

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -15,4 +15,47 @@ public class QuestManager : MonoBehaviour
 
         questMarkersComplete = new bool[questMarkerNames.Length];
     }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log(CheckIfComplete("Quest test"));
+            MarkQuestComplete("Quest test");
+            MarkQuestIncomplete("Fight the demon");
+        }
+    }
+
+    public int GetQuestNumber(string questToFind)
+    {
+        for (int i = 0; i < questMarkerNames.Length; i++)
+        {
+            if (questMarkerNames[i] == questToFind)
+            {
+                return i;
+            }
+        }
+
+        Debug.LogError("Quest " + questToFind + " does not exist.");
+        return 0;
+    }
+
+    public bool CheckIfComplete(string questToCheck)
+    {
+        if (GetQuestNumber(questToCheck) != 0)
+        {
+            return questMarkersComplete[GetQuestNumber(questToCheck)];
+        }
+        return false;
+    }
+
+    public void MarkQuestComplete(string questToMark)
+    {
+        questMarkersComplete[GetQuestNumber(questToMark)] = true;
+    }
+
+    public void MarkQuestIncomplete(string questToMark)
+    {
+        questMarkersComplete[GetQuestNumber(questToMark)] = false;
+    }
 }
