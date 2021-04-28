7f55de0a22e85883cd85047cdf2278bef764ff12
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-04-28 07:51:08 -0400

    Completed Lesson 71

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public string[] questMarkerNames;
+    public bool[] questMarkersComplete;
+
+    public static QuestManager instance;
+
+    void Start()
+    {
+        instance = this;
+
+        questMarkersComplete = new bool[questMarkerNames.Length];
+    }
+}
