76f8a3eaa37bc4de8ab0e9efc37994df9d2339cc
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-04-30 08:56:34 -0400

    Completed Lesson 84

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -171,7 +171,7 @@ public class GameManager : MonoBehaviour
 
     public void SaveData()
     {
-        PlayerPrefs.SetString("Current Scene", SceneManager.GetActiveScene().name);
+        PlayerPrefs.SetString("Current_Scene", SceneManager.GetActiveScene().name);
         PlayerPrefs.SetFloat("Player_Position_x", PlayerController.instance.transform.position.x);
         PlayerPrefs.SetFloat("Player_Position_y", PlayerController.instance.transform.position.y);
         PlayerPrefs.SetFloat("Player_Position_z", PlayerController.instance.transform.position.z);
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string newGameScene;
+    public GameObject continueButton;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Current_Scene"))
+        {
+            continueButton.SetActive(true);
+        }
+        else
+        {
+            continueButton.SetActive(false);
+        }
+    }
+
+    public void Continue()
+    {
+
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
