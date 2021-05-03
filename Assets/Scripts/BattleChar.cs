415cf7f7ecfc2b3bc1746ab16c62b84876aa705f
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-05-03 07:52:47 -0400

    Completed Lesson 88

---
+++ Assets/Scripts/BattleChar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleChar : MonoBehaviour
+{
+    public bool isPlayer;
+    public string[] movesAvailable;
+
+    public string charName;
+    public int currentHP;
+    public int maxHP;
+    public int currentMP;
+    public int maxMP;
+    public int strength;
+    public int defence;
+    public int weaponPower;
+    public int armorPower;
+    public bool hasDied;
+
+}
