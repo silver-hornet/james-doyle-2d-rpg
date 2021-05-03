6583b0db3cd8aafb4a7664e67644877e63ff433e
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-05-03 08:40:37 -0400

    Completed Lesson 90

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -16,6 +16,8 @@ public class BattleManager : MonoBehaviour
     public BattleChar[] playerPrefabs;
     public BattleChar[] enemyPrefabs;
 
+    public List<BattleChar> activeBattlers = new List<BattleChar>();
+
     void Start()
     {
         instance = this;
@@ -39,6 +41,49 @@ public class BattleManager : MonoBehaviour
             transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, transform.position.z);
             battleScene.SetActive(true);
             AudioManager.instance.PlayBGM(0);
+
+            for (int i = 0; i < playerPositions.Length; i++)
+            {
+                if (GameManager.instance.playerStats[i].gameObject.activeInHierarchy)
+                {
+                    for (int j = 0; j < playerPrefabs.Length; j++)
+                    {
+                        if (playerPrefabs[j].charName == GameManager.instance.playerStats[i].charName)
+                        {
+                            BattleChar newPlayer = Instantiate(playerPrefabs[j], playerPositions[i].position, playerPositions[i].rotation);
+                            newPlayer.transform.parent = playerPositions[i];
+                            activeBattlers.Add(newPlayer);
+
+                            CharStats thePlayer = GameManager.instance.playerStats[i];
+                            activeBattlers[i].currentHP = thePlayer.currentHP;
+                            activeBattlers[i].maxHP = thePlayer.maxHP;
+                            activeBattlers[i].currentMP = thePlayer.currentMP;
+                            activeBattlers[i].maxMP = thePlayer.maxMP;
+                            activeBattlers[i].strength = thePlayer.strength;
+                            activeBattlers[i].defence = thePlayer.defence;
+                            activeBattlers[i].weaponPower = thePlayer.weaponPower;
+                            activeBattlers[i].armorPower = thePlayer.armorPower;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < enemiesToSpawn.Length; i++)
+            {
+                if (enemiesToSpawn[i] != "")
+                {
+                    for (int j = 0; j < enemyPrefabs.Length; j++)
+                    {
+                        if (enemyPrefabs[j].charName == enemiesToSpawn[i])
+                        {
+                            BattleChar newEnemy = Instantiate(enemyPrefabs[j], enemyPositions[i].position, enemyPositions[i].rotation);
+                            newEnemy.transform.parent = enemyPositions[i];
+                            activeBattlers.Add(newEnemy);
+                        }
+
+                    }
+                }
+            }
         }
     }
 }
