484f29ce1825bfd96695c3c595e2c8408c17d33c
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-05-07 08:20:45 -0400

    Completed Lesson 102

---
+++ Assets/Scripts/BattleMagicSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleMagicSelect : MonoBehaviour
+{
+    public string spellName;
+    public int spellCost;
+    public Text nameText;
+    public Text costText;
+
+    public void Press()
+    {
+        if (BattleManager.instance.activeBattlers[BattleManager.instance.currentTurn].currentMP >= spellCost)
+        {
+            BattleManager.instance.magicMenu.SetActive(false);
+            BattleManager.instance.OpenTargetMenu(spellName);
+            BattleManager.instance.activeBattlers[BattleManager.instance.currentTurn].currentMP -= spellCost;
+        }
+        else
+        {
+            // let player know there is not enough MP
+        }
+    }
+}
