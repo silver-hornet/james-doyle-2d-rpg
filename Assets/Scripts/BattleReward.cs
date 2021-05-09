0086da7fc371fc3ca628936727d91e56473b8094
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-05-09 12:23:49 -0400

    Completed Lesson 110

---
+++ Assets/Scripts/BattleReward.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleReward : MonoBehaviour
+{
+    public static BattleReward instance;
+
+    public Text xpText;
+    public Text itemText;
+    public GameObject rewardScreen;
+
+    public string[] rewardItems;
+    public int xpEarned;
+
+    void Start()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            OpenRewardScreen(54, new string[] { "Iron Sword", "Iron Armor" });
+        }
+    }
+
+    public void OpenRewardScreen(int xp, string[] rewards)
+    {
+        xpEarned = xp;
+        rewardItems = rewards;
+
+        xpText.text = "Everyone earned " + xpEarned + " xp!";
+        itemText.text = "";
+
+        for (int i = 0; i < rewardItems.Length; i++)
+        {
+            itemText.text += rewards[i] + "\n";
+        }
+
+        rewardScreen.SetActive(true);
+    }
+
+    public void CloseRewardScreen()
+    {
+
+    }
+}
