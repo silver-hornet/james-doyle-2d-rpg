c394df5f7b4a9236635927b18e6b607a7b4526fe
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-05-10 07:53:25 -0400

    Completed Lesson 112

---
+++ Assets/Scripts/BattleStarter.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleStarter : MonoBehaviour
+{
+    public BattleType[] potentialBattles;
+    public bool activateOnEnter;
+    public bool activateOnStay;
+    public bool activateOnExit;
+
+    bool inArea;
+    public float timeBetweenBattles = 10f;
+    float betweenBattleCounter;
+
+    public bool deactivateAfterStarting;
+
+    void Start()
+    {
+        betweenBattleCounter = Random.Range(timeBetweenBattles * 0.5f, timeBetweenBattles * 1.5f);
+    }
+    void Update()
+    {
+        if (inArea && PlayerController.instance.canMove)
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                betweenBattleCounter -= Time.deltaTime;
+            }
+
+            if (betweenBattleCounter <= 0)
+            {
+                betweenBattleCounter = Random.Range(timeBetweenBattles * 0.5f, timeBetweenBattles * 1.5f);
+                StartCoroutine(StartBattleCo());
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (activateOnEnter)
+            {
+                StartCoroutine(StartBattleCo());
+            }
+            else
+            {
+                inArea = true;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (activateOnExit)
+            {
+                StartCoroutine(StartBattleCo());
+            }
+            else
+            {
+                inArea = false;
+            }
+        }
+    }
+
+    public IEnumerator StartBattleCo()
+    {
+        UIFade.instance.FadeToBlack();
+        GameManager.instance.battleActive = true;
+
+        int selectedBattle = Random.Range(0, potentialBattles.Length);
+
+        BattleManager.instance.rewardItems = potentialBattles[selectedBattle].rewardItems;
+        BattleManager.instance.rewardXP = potentialBattles[selectedBattle].rewardXP;
+
+        yield return new WaitForSeconds(1.5f);
+
+        BattleManager.instance.BattleStart(potentialBattles[selectedBattle].enemies);
+        UIFade.instance.FadeFromBlack();
+
+        if (deactivateAfterStarting)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
